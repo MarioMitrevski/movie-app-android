cf6724aa662f6bf5ac8e79777b65434a3e9df45a
MarioMitrevskii <mario.mitrevski@intertec.io>
2022-03-04 00:44:20 +0100

    Retrofit and RxJava setup

---
+++ app/src/main/java/movieapp/app/datasources/ApiModule.kt
@@ -0,0 +1,75 @@
+package movieapp.app.datasources
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import movieapp.app.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class ApiModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor()
+        .apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        class ApiKeyInterceptor : Interceptor {
+            @Throws(IOException::class)
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val originalRequest = chain.request()
+                val newUrl = originalRequest.url.newBuilder().addQueryParameter("api_key", BuildConfig.THE_MOVIE_DB_API_KEY).build()
+
+                val requestBuilder = originalRequest.newBuilder().url(newUrl)
+                val newRequest = requestBuilder.build()
+                return chain.proceed(newRequest)
+            }
+        }
+
+        val okHttpClientBuilder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            okHttpClientBuilder.addInterceptor(httpLoggingInterceptor)
+        }
+
+        return okHttpClientBuilder
+            .addInterceptor(ApiKeyInterceptor())
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRxCallAdapter(): RxJava3CallAdapterFactory {
+        return RxJava3CallAdapterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient,
+        rxJava3CallAdapterFactory: RxJava3CallAdapterFactory
+    ): Retrofit {
+
+        return Retrofit.Builder()
+            .client(client)
+            .baseUrl(BuildConfig.BASE_URL)
+            .addCallAdapterFactory(rxJava3CallAdapterFactory)
+            .build()
+    }
+}
