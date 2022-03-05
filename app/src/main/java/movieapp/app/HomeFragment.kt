214f5590a3bd9d6e740f5c0cef78fcb7568064d6
MarioMitrevskii <mario.mitrevski@intertec.io>
2022-03-05 20:11:56 +0100

    Set welcome message

--- app/src/main/java/movieapp/app/HomeFragment.kt
+++ app/src/main/java/movieapp/app/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import movieapp.app.databinding.FragmentHomeBinding
+import java.time.LocalTime
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -20,6 +21,27 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setWelcomeMessage()
+    }
+
+    private fun setWelcomeMessage() {
+        val now = LocalTime.now()
+        binding.title.text = when {
+            now.isAfter(LocalTime.of(4, 0)) and now.isBefore(LocalTime.of(12, 0)) -> {
+                getString(R.string.good_morning)
+            }
+            now.isAfter(LocalTime.of(12, 0)) and now.isBefore(LocalTime.of(20, 0)) -> {
+                getString(R.string.good_afternoon)
+            }
+            else -> {
+                getString(R.string.good_evening)
+            }
+        }
     }
 
     override fun onDestroyView() {
